4b68e671c9c0d2d924ded497250dfbf26974e739
BenjaminMuhlestein <100793078+BenjaminMuhlestein@users.noreply.github.com>
2022-05-21 10:45:00 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+document.addEventListener('assets/green-character/static.gif', function(east){
+ 
+    if(east.repeat) return; 
+
+    if(east.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(east.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(east.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(east.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    callback(direction)
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+    callback(direction)
+})
+
+
